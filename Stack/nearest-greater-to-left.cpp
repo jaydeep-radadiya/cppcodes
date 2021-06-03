b0633b37eef902913c1aca00d7bb9fc2e380bcff
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-06-03 21:52:06 +0530

    stack based program added

---
+++ Stack/nearest-greater-to-left.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int> arr(n);
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+
+	vector<int> ans;
+	stack<int> s;
+
+	for(int i=0;i<n;i++){
+		if(s.size()==0)
+			ans.push_back(-1);
+		else if(s.size()>0 and s.top()>arr[i])
+			ans.push_back(s.top());
+		else if(s.size()>0 and s.top()<=arr[i]){
+			while(s.size()>0 and s.top()<=arr[i])
+				s.pop();
+			if(s.size()==0)
+				ans.push_back(-1);
+			else
+				ans.push_back(s.top());
+		}
+		s.push(arr[i]);
+	}
+
+	for(int ele : ans)
+		cout<<ele<<" ";
+
+	return 0;
+}
