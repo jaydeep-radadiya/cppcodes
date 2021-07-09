f70db8090f83eeae990174110f02eb37614b15c5
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-07-09 23:37:53 +0530

    codeforces solution

---
+++ Codeforces/domino-piling.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+
+void fastIO(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+}
+
+#define ll long long
+#define mod 1000000007
+#define ff first
+#define ss second
+#define mid(l,r) (l+(r-l)/2)
+
+int main(){
+	fastIO();
+
+	int m,n;
+	cin>>m>>n;
+	cout<<(m*n)/2<<"\n";
+
+	return 0;
+}
