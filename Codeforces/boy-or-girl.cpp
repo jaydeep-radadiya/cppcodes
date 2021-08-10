70a3b7226f5890a7d10855f1eb307bf757ab5cde
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-08-10 23:59:20 +0530

    codeforces solution

---
+++ Codeforces/boy-or-girl.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s;
+	cin>>s;
+
+	unordered_set<char> ss;
+	for(int i=0;i<s.size();i++){
+		ss.insert(s[i]);
+	}
+
+	if(ss.size()%2==0)
+		cout<<"CHAT WITH HER!"<<"\n";
+	else
+		cout<<"IGNORE HIM!"<<"\n";
+
+	return 0;
+}
