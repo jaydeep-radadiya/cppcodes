04c5ea482282eb74ec6b4dbf882525eed02fb60e
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-09-11 23:04:47 +0530

    new method added

--- Fraz/best-time-to-buy-and-sell-stock.cpp
+++ Fraz/best-time-to-buy-and-sell-stock.cpp
@@ -1,3 +1,21 @@
+// first method
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxpro = 0;
+        
+        for(int i=0;i<prices.size()-1;i++){
+            for(int j=i+1;j<prices.size();j++){
+                if(prices[j]-prices[i]>maxpro)
+                    maxpro = prices[j]-prices[i];
+            }
+        }
+        
+        return maxpro;
+    }
+};
+
+// second method
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
