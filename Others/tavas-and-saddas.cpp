e7ac65b0bf6db65af143bbfcfa1b4ee53a7baed4
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-11-24 20:45:46 +0530

    based on bit masking

---
+++ Others/tavas-and-saddas.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+	string s;
+	cin>>s;
+	int len = s.size();
+	int ans = (1<<len)-2;
+	
+	for(int i=len-1,j=0;i>=0;i--,j++){
+		if(s[i]=='7'){
+			ans += 1<<j;
+		}
+	}
+	cout<<ans+1;
+	return 0;
+}
